a19d66ac8d6a685581ef753a971f1f603d47f815
Satoshi Konno <skonno@yahoo-corp.jp>
2025-03-13 13:02:21 +0900

    Add ThreadTest()

---
+++ test/ThreadTest.cpp
@@ -0,0 +1,51 @@
+/******************************************************************
+ *
+ * Copyright (C) 2025 The Cyber Garage Portable Runtime for C Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************/
+
+#include <boost/test/unit_test.hpp>
+#include <boost/thread.hpp>
+
+#include <cgpr/util/thread.h>
+#include <cgpr/util/timer.h>
+
+const int THREAD_TEST_LOOP_NUM = 10;
+
+void test_theread_func(CGThread* thread)
+{
+  int* test_counter = (int*)cg_thread_getuserdata(thread);
+  for (int n = 0; n < THREAD_TEST_LOOP_NUM; n++) {
+    (*test_counter)++;
+  }
+}
+
+BOOST_AUTO_TEST_CASE(ThreadTest)
+{
+  CGThread* thread = cg_thread_new();
+
+  int test_counter = 0;
+  cg_thread_setaction(thread, test_theread_func);
+  cg_thread_setuserdata(thread, &test_counter);
+
+  BOOST_REQUIRE_EQUAL(cg_thread_start(thread), true);
+  while (test_counter != THREAD_TEST_LOOP_NUM) {
+    cg_sleep(100);
+  }
+  BOOST_REQUIRE_EQUAL(test_counter, THREAD_TEST_LOOP_NUM);
+  BOOST_REQUIRE_EQUAL(cg_thread_stop(thread), true);
+
+  cg_thread_delete(thread);
+}
